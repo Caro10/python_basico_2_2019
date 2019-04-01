747ee5c6dbb456f9024cf12234a0441a36043a76
Caro10 <carolinablair10@gmail.com>
2019-04-01 01:33:50 -0600

    me gusta "el tarro" a

--- semana1/practica_2.py
+++ semana1/practica_2.py
@@ -1,4 +1,4 @@
-# Con numeral hacemos comentarios respecto al código que voy a escibir.
+﻿# Con numeral hacemos comentarios respecto al código que voy a escibir.
 # En esta practica escribiriamos dos lineas.
 
 #Se puede tambien con tres comillas
@@ -7,7 +7,7 @@
 print("Hola mundo")
 print("buenos dias")
 #Varias lineas en un solo print
-print("primer linea\nmi segunda linea") #juntar con el comando hace que se quite el primer espacio.
+print("primer linea\nmi segunda linea") #juntar con el caracter hace que se quite el primer espacio.
 
 print ("hola mundo")  #escribir de manera ordenada error de identacion, en pyt,
 
